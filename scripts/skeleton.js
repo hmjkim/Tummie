6b4d742e5c4abf704f1d0e1c632dd98460f0493b
HEI YEUNG SZE <hsze6@my.bcit.ca>
2024-10-30 14:48:29 -0700

    Add a header for pages before login

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,6 +3,7 @@
 // (navbar, footer, and other things) into html doc.
 //---------------------------------------------------
 function loadSkeleton() {
+    $('#headerBeforeLogin').load('./partials/headerBeforeLogin.html');
     $('#headerContent').load('./partials/header.html');
     $('#footerContent').load('./partials/footer.html');
     $('#stickyMobileNav').load('./partials/sticky_mobile_nav.html');
