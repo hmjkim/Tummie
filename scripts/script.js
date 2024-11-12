59efd74185f32395f2e021de13645b6ba24f66f3
Heather Kim <heathermjkim@gmail.com>
2024-11-11 23:06:09 -0800

    Add go back button on add food page

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,29 @@
-function sayHello() {
-    
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      })
+      .catch((error) => {
+        // An error happened.
+      });
 }
-//sayHello();
+
+//------------------------------------------------
+// Go back to previous page
+//-------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+    const goBackBtn = document.getElementById("go-back");
+    if (!goBackBtn) {
+        return;
+    }
+    goBackBtn.addEventListener("click", () => {
+        history.back();
+      });
+})
