7924ce344149b12d8a8a2c62ae4e4a633a383f10
Heather Kim <heathermjkim@gmail.com>
2024-11-19 02:35:02 -0800

    Update sticky nav bar links to be highlighted if current page

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -7,11 +7,41 @@ function loadSkeleton() {
     if (user) {
       $('#headerContent').load('./partials/header.html');
       $('#footerContent').load('./partials/footer.html');
-      $('#stickyMobileNav').load('./partials/sticky_mobile_nav.html');
+      $('#stickyMobileNav').load('./partials/sticky_mobile_nav.html', () => {
+        updateActiveNavLink()
+      });
     } 
   });
   }
 
 $(document).ready(function(){
     loadSkeleton(); 
+
 });
+
+
+//---------------------------------------------------
+// Update sticky nav bar links to be highlighted if current page
+//---------------------------------------------------
+function updateActiveNavLink() {
+  const navLinks = document.querySelectorAll('#stickyMobileNav a');
+  const currentURL = window.location.pathname;
+
+  navLinks.forEach((link) => {
+    if (currentURL.includes(link.getAttribute('href'))) {
+      let linkText = link.innerText;
+      link.classList.add('active');
+      link.querySelector('img').src = `./images/icons/${slugify(linkText)}-active.svg`
+    }
+  });
+}
+
+function slugify(str) {
+  str = str.replace(/^\s+|\s+$/g, ""); // trim leading/trailing white space
+  str = str.toLowerCase(); // convert string to lowercase
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove any non-alphanumeric characters
+    .replace(/\s+/g, "-") // replace spaces with hyphens
+    .replace(/-+/g, "-"); // remove consecutive hyphens
+  return str;
+}
