73c3189d9808690ad35dbcdc0fd5da22ef90222f
HEI YEUNG SZE <hsze6@my.bcit.ca>
2024-12-01 02:44:41 -0800

    Remove unused auto-written field

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -20,7 +20,6 @@ var uiConfig = {
                 db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                     name: user.displayName,                    //"users" collection
                     email: user.email,                         //with authenticated user's ID (user.uid)
-                    notification_preference: true,
                     default_sorting: "defaultSortByDate"
                 }).then(function () {
                     console.log("New user added to firestore");
