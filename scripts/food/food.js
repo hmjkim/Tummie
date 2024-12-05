d14856506b36b1db7dd91b8602cf153494e799a2
Heather Kim <heathermjkim@gmail.com>
2024-12-05 09:32:43 -0800

    Fix preview file function ensure image upload

--- scripts/food/food.js
+++ scripts/food/food.js
@@ -4,6 +4,8 @@ import {
   displayFoodItemsByName,
 } from "./sort-food.js";
 
+import { previewFile, slugify, convertToTitleCase, getURLParams, setURLParams } from "./foodHelpers.js"
+
 // import { updateSortingMethod } from "./sort.js";
 
 var currentUser;
@@ -741,52 +743,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// Preview Uploaded Image File before server submission
-function previewFile(input) {
-  var file = $("input[type=file]").get(0).files[0];
 
-  if (file) {
-    var reader = new FileReader();
-
-    reader.onload = function () {
-      $("#previewImg").attr("src", reader.result);
-    };
-
-    reader.readAsDataURL(file);
-  }
-}
-
-function slugify(str) {
-  str = str.replace(/^\s+|\s+$/g, ""); // trim leading/trailing white space
-  str = str.toLowerCase(); // convert string to lowercase
-  str = str
-    .replace(/[^a-z0-9 -]/g, "") // remove any non-alphanumeric characters
-    .replace(/\s+/g, "-") // replace spaces with hyphens
-    .replace(/-+/g, "-"); // remove consecutive hyphens
-  return str;
-}
-
-function convertToTitleCase(str) {
-  if (!str) {
-    return "";
-  }
-  return str.toLowerCase().replace(/\b\w/g, (s) => s.toUpperCase());
-}
-
-function getURLParams(key) {
-  const params = new URL(window.location.href).searchParams; // get the url from the search bar and its search parameters
-  const value = params.get(key);
-
-  return value;
-}
-
-function setURLParams(key, value) {
-  let url = new URL(window.location.href);
-  url.searchParams.set(key, value); // get the search parameter and add a new query string with key value
-
-  // Go to url without reloading
-  history.pushState(null, "", url);
-}
 
 function populateFoodInfo() {
   firebase.auth().onAuthStateChanged((user) => {
@@ -892,4 +849,14 @@ function updateFoodItem(userID) {
     });
 }
 
+
+//------------------------------------------------
+// Upload Image on Add Food/ Edit Item pages
+//-------------------------------------------------
+const uploadImageInput = document.querySelector("#imageUpload");
+if (uploadImageInput) {
+  uploadImageInput.addEventListener("change", () => {
+    previewFile()
+  })
+}
 export { calculateTimeLeft, determineRemainingDaysMessage };
