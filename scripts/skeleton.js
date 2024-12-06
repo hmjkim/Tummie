b36b89af16df08eb3d24b3ce119279586a5446b8
Heather Kim <heathermjkim@gmail.com>
2024-12-06 14:13:17 -0800

    Fix nav highlight on add and edit pages

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -5,19 +5,28 @@
 function loadSkeleton() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      // https://api.jquery.com/jQuery.when/
-      // Ensure header, footer, and mobile nav are loaded first before calling the callback function (returning Promise)
-      var d1 = $.Deferred();
-      var d2 = $.Deferred();
-      var d3 = $.Deferred();
+        // https://api.jquery.com/jQuery.when/
+        // Ensure header, footer, and mobile nav are loaded first before calling the callback function (returning Promise)
+        var d1 = $.Deferred();
+        var d2 = $.Deferred();
+        var d3 = $.Deferred();
 
-      $("#headerContent").load("../partials/header.html", () => { d1.resolve()});
-      $("#footerContent").load("../partials/footer.html", () => { d2.resolve()}),
-      $("#stickyMobileNav").load("../partials/stickyMobileNav.html", () => { d3.resolve()})
+        $("#headerContent").load("../partials/header.html", () => { d1.resolve()});
 
-      $.when(d1, d2, d3).done(() => {
-        updateActiveNavLink();
-      });
+        // For addFood and eachFood pages
+        if( $('#stickyMobileNav').length == 0 ){
+          $.when(d1).done(() => {
+            updateActiveNavLink();
+          });
+        } else {
+          
+          $("#footerContent").load("../partials/footer.html", () => { d2.resolve()});
+          $("#stickyMobileNav").load("../partials/stickyMobileNav.html", () => { d3.resolve()});
+  
+        $.when(d1, d2, d3).done(() => {
+          updateActiveNavLink();
+        });
+      }
     }
   });
 }
@@ -30,18 +39,20 @@ $(document).ready(function () {
 // Update sticky nav bar links to be highlighted if current page
 //---------------------------------------------------
 function updateActiveNavLink() {
+  console.log(updateActiveNavLink);
   const navLinks = document.querySelectorAll("#stickyMobileNav a");
   const desktopNavLinks = document.querySelectorAll(
     "#headerContent .nav-item a"
   );
+  console.log('navLinks', navLinks)
   const currentURL = window.location.pathname;
     [...navLinks, ...desktopNavLinks].forEach((link) => {
       let linkURL = link.getAttribute('href');
+      let parentPath = currentURL.split('/')[1];
 
       // Create a parent and child page relationship 
       // eg. recipes.html (parent) , recipes/eachRecipe.html
       // eg. child page under recipes will have recipe link highlighted
-      let parentPath = currentURL.split('/')[1];
       if (linkURL.includes(parentPath)) {
         let linkText = link.innerText;
         link.classList.add('active');
