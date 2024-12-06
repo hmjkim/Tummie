89d627af6c31e6230083673ec6ea3f746f23aefd
Heather Kim <heathermjkim@gmail.com>
2024-12-06 02:16:52 -0800

    Ensure jquery loads all files before invoking callback function, desktop link highlight

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -5,10 +5,18 @@
 function loadSkeleton() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      $('#headerContent').load('./partials/header.html');
-      $('#footerContent').load('./partials/footer.html');
-      $('#stickyMobileNav').load('./partials/sticky_mobile_nav.html', () => {
-        updateActiveNavLink()
+      // https://api.jquery.com/jQuery.when/
+      // Ensure header, footer, and mobile nav are loaded first before calling the callback function (returning Promise)
+      var d1 = $.Deferred();
+      var d2 = $.Deferred();
+      var d3 = $.Deferred();
+
+      $("#headerContent").load("../partials/header.html", () => { d1.resolve()});
+      $("#footerContent").load("../partials/footer.html", () => { d2.resolve()}),
+      $("#stickyMobileNav").load("../partials/sticky_mobile_nav.html", () => { d3.resolve()})
+
+      $.when(d1, d2, d3).done(() => {
+        updateActiveNavLink();
       });
     }
   });
@@ -16,26 +24,47 @@ function loadSkeleton() {
 
 $(document).ready(function () {
   loadSkeleton();
-
 });
-
 
 //---------------------------------------------------
 // Update sticky nav bar links to be highlighted if current page
 //---------------------------------------------------
 function updateActiveNavLink() {
-  const navLinks = document.querySelectorAll('#stickyMobileNav a');
+  const navLinks = document.querySelectorAll("#stickyMobileNav a");
+  const desktopNavLinks = document.querySelectorAll(
+    "#headerContent .nav-item a"
+  );
+  const recipesNavLinks = document.querySelectorAll(".js-recipes-link");
+
+  console.log("desktop", desktopNavLinks);
+  console.log("recipesNavLink", recipesNavLink);
   const currentURL = window.location.pathname;
 
-  if (currentURL == '/eachRecipe.html' || currentURL == '/favourite.html' || currentURL == '/eachFavouriteRecipe.html') {
-    recipesNavLink.classList.add('active');
-    recipesNavLink.querySelector('img').src = `./images/icons/recipes-active.svg`
+  if (
+    currentURL == "/eachRecipe.html" ||
+    currentURL == "/favourite.html" ||
+    currentURL == "/eachFavouriteRecipe.html"
+  ) {
+    recipesNavLinks.forEach((link) => {
+      link.classList.add("active");
+      let linkIcon = link.querySelector("img");
+      if (linkIcon) {
+        linkIcon.src = `./images/icons/recipes-active.svg`;
+      }
+    })
   } else {
-    navLinks.forEach((link) => {
-      if (link.getAttribute('href').includes(currentURL)) {
+    [...navLinks, ...desktopNavLinks].forEach((link) => {
+      console.log("link ", link);
+      console.log("current url ", currentURL);
+      console.log("href url ", link.getAttribute("href"));
+      console.log(link.getAttribute("href").includes(currentURL));
+      if (link.getAttribute("href").includes(currentURL)) {
         let linkText = link.innerText;
-        link.classList.add('active');
-        link.querySelector('img').src = `./images/icons/${slugify(linkText)}-active.svg`
+        link.classList.add("active");
+        let linkIcon = link.querySelector("img");
+        if (linkIcon) {
+          linkIcon.src = `./images/icons/${slugify(linkText)}-active.svg`;
+        }
       }
     });
   }
@@ -46,6 +75,6 @@ function slugify(str) {
   str = str.toLowerCase(); // convert string to lowercase
   str = str
     .replace(/[^a-z0-9 -]/g, "") // remove any non-alphanumeric characters
-    .replace(/\s+/g, "") // remove spaces
+    .replace(/\s+/g, ""); // remove spaces
   return str;
 }
